9f02f10b50cc604eb9f4c73f3c75828f56002ad7
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-02-11 21:22:48 -0300

    add task

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ class Task:
         self.description = description
         self.complete = complete
 
-class TaskManeger:
+class TaskManager:
     def __init__ (self):
         self.lista = []
+    
+    def add_task (self, title, description):
+        lista  = Task(title, description)
+        self.append(lista)
