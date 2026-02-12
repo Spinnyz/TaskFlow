37bebafe151b83c989c7df9e565e4b2d0dde3f9d
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-02-12 20:44:33 -0300

    list_tasks

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Task:
-    def __init__ (self, title, description, complete = False):
+    def __init__ (self, title, description, completed = False):
         self.title = title
         self.description = description
-        self.complete = complete
+        self.completed = completed
 
 class TaskManager:
     def __init__ (self):
@@ -11,8 +11,14 @@ class TaskManager:
     def add_task (self, title, description):
         task  = Task(title, description)
         self.lista.append(task)
+        return task
+    
+    def list_tasks(self):
+        for indice, tarefa  in enumerate(self.lista):
+            status = "Concluida" if tarefa.completed else "Pendente"
+            print (f"{indice} - {tarefa.title} - {tarefa.description} - {status}")
 
-t1 = Task("Pyhton", "lala", True)
-print (t1.title)
-print (t1.description)
-print (t1.complete)
+
+t1 = TaskManager()
+t1.add_task("Estuda", "Python")
+t1.list_tasks()
